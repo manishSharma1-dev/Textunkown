91a035d7ee3ab31e3eddcdff1883cf71eb5f939f
manish sharma dev <manishvsharma1@gmail.com>
2024-09-06 10:47:22 -1200

    added middleware file, congfigured sign-in logic using custom jwt configuration

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page(){
+    return (
+        <div>
+        <p>This page will be used for the sign-in feature</p>
+        </div>
+    )
+}
+
+export default page
