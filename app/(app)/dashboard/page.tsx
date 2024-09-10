5ae2d4b02d61c45c20d506996a750004f369d60d
manish sharma dev <manishvsharma1@gmail.com>
2024-09-09 12:40:50 -1200

    Created User DashBoard/ messages part

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React,{ useCallback, useEffect, useState } from 'react'
 import { Message } from '@/models/user.model'
 import { useToast } from '@/hooks/use-toast'
@@ -7,16 +9,21 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios from 'axios'
 import { ApiResponse } from '@/types/Apiresponse'
+import { Button } from '@/components/ui/button'
+import { Switch } from '@/components/ui/switch'
+import { Separator } from '@/components/ui/separator'
+import { Loader2, RefreshCcw } from 'lucide-react'
+import { MessageCard } from '@/components/MessageCard'
 
 
 export default function page() {
-    const [ Message, setMessages ] = useState<Message[]>([])
+    const [ messages, setMessages ] = useState<Message[]>([])
     const [isloading,setIsLoading] = useState(false)
     const [isswitchLoading,setIsSwitchLoading] = useState(false)
     const { toast } = useToast()
 
-    const handelDeleteMessages = (messageId:string) => {
-         setMessages(Message.filter((message) => message._id !== messageId))
+    const handelDeleteMessages = (messageId : string ) => {
+         setMessages(messages.filter((message) => message._id !== messageId))
     }
 
     const { data: session } = useSession()
@@ -93,26 +100,28 @@ export default function page() {
     }, [session,setValue,fetchAcceptMessages,fetchAllMessages])
 
     const Handle_Switch_Change_Between_Accept_OrDecline_Message  = async() => {
-
-      setIsSwitchLoading(false)
-
+     
       try {
+
+        setIsSwitchLoading(false)
+
         const response  = await axios.post<ApiResponse>('/api/accept-message',{
           acceptMessages : !acceptMessages
         })
 
-        if(response){
-          toast({
-            title : "Success",
-            description : "Access granted"
-          })
-        }
+        setValue('acceptMessages',!acceptMessages)
+
+        toast({
+          title : "Success",
+          description : "Access granted"
+        })
   
       } catch (error) {
 
         toast({
           title : "Switching Failed",
-          description : `An Error Occured while access to the switch ${error}`
+          description : `An Error Occured while access to the switch ${error}`,
+          variant : 'destructive'
         })
 
         throw new Error('An Error Occured while Switched between access')
@@ -122,10 +131,75 @@ export default function page() {
 
     }
 
+
+    if(!session || !session.user){
+      return <div>login first Pls</div>
+    }
+
+    const { username } = session.user
+    const baseUrl = `${window.location.protocol}//${window.location.host}`
+    const profileUrl = `${baseUrl}/u/${username}`
+
+    const copytoClipBoard = () => {
+      navigator.clipboard.writeText(profileUrl)
+      toast({
+        title : "Success",
+        description : "Copied Successfully"
+      })
+    }
+
+
   return (
     <div>
-      
+      <h1>User DashBoard</h1>
+      <p>Copy your Profile url</p>
+
+      <div>
+        <input type='text' value={profileUrl} />
+        <Button onClick={copytoClipBoard}>Copy</Button>
+      </div>
+
+      <Switch 
+        {...register('acceptMessages')}
+        checked={acceptMessages}
+        onCheckedChange={Handle_Switch_Change_Between_Accept_OrDecline_Message}
+        disabled={isswitchLoading}
+      />
+      <span>Accept Messageg: {acceptMessages?'On':'Off'}</span>
+
+      <Separator />
+
+      <Button
+        variant={'outline'}
+        onClick={(e) => {
+          e.preventDefault();
+          fetchAllMessages(true)
+        }}
+      >{
+        isloading? (
+          <Loader2 className='h-4 w-4 animate-spin' />
+        ) : (
+          <RefreshCcw className='h-4 w-4' />
+        )
+      }</Button>
+
+
+      <div>
+
+       {messages.length > 0 ? (
+        messages.map((message,index) => (
+          <MessageCard 
+            key={index}
+            message={message}
+            onMessageDelete={handelDeleteMessages}
+          />
+        ))
+       ) : (
+         <p>No message to display</p>
+       )}
+
+      </div>
+
     </div>
   )
 }
- 
